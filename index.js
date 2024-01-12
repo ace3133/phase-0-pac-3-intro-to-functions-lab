c20cbfc3b43c0407f1b4bd20195aca0f2b759686
Hass <hassmroueh@gmail.com>
2024-01-12 13:28:36 -0500

    Intro to functions lab

---
+++ index.js
@@ -0,0 +1,53 @@
+function shout(string) {
+  // todo
+}
+function shout(string) {
+  return string;
+}
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  // todo
+}
+function whisper(string) {
+  return string;
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  // todo
+}
+function logShout(string) {
+  console.log (string);
+}
+function logShout(string) {
+  console.log (string).toUpperCase();
+}
+function logShout(string) {
+  console.log("HELLO"); 
+}
+function logWhisper(string) {
+  // todo
+}
+function logWhisper(string) {
+  console.log (string);
+}
+function LogWhisper(string) {
+  console.log (string).toLowerCase();
+}
+function logWhisper(string) {
+  console.log("hello")
+}
+function sayHiToHeadphonedRoommate(str) {
+  if (str.toLowerCase() === str) {
+    return "I can't hear you!";
+  } else if (str.toUpperCase() === str) {
+    return "YES INDEED!";
+  } else if (str === "Let's have dinner together!") {
+    return "I would love to!";
+  } else {
+    return "Hello!";
+  }
+}
